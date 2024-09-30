c00d753151f91e9c03006cc1c887bba236fb17c5
Drewplonq <it@goplonq.com>
2024-09-30 22:36:31 +0300

    Add files via upload

---
+++ count-vm.tf
@@ -0,0 +1,35 @@
+data "yandex_compute_image" "ubuntu" {
+  family = var.vm_fam
+}
+resource "yandex_compute_instance" "web" {
+   count       = var.counts
+   name        = "${var.vms_name}-${count.index + 1}"
+   platform_id = var.vms_resources.platform_id
+   depends_on = [yandex_compute_instance.db]
+
+   resources {
+    cores         = var.vms_resources.cores
+    memory        = var.vms_resources.memory
+    core_fraction = var.vms_resources.core_fraction
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+     
+    }
+  }
+
+    metadata = {
+    serial-port-enable = 1
+    ssh-keys           = "ubuntu:${var.ssh_key}"
+  }
+
+  scheduling_policy { preemptible = var.vms_resources.preemptible }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    security_group_ids = [yandex_vpc_security_group.example.id]
+    nat = var.vms_resources.nat
+  }
+}  
---
+++ for_each-vm.tf
@@ -0,0 +1,37 @@
+data "yandex_compute_image" "ubuntuDB" {
+  family = var.vm_fam
+}
+resource "yandex_compute_instance" "db" {
+  for_each = {
+    main = var.each_vm[0]
+    replica = var.each_vm[1]
+  }
+  name        = "${each.value.vm_name}"
+  platform_id = var.vms_resources.platform_id
+
+
+  resources {
+    cores         = "${each.value.cpu}"
+    memory        = "${each.value.ram}"
+    core_fraction = var.vms_resources.core_fraction
+  }
+
+  boot_disk {
+    initialize_params {
+        image_id = data.yandex_compute_image.ubuntu.image_id
+       
+    }
+  }
+
+  metadata = {
+    serial-port-enable = 1
+    ssh-keys           = var.ssh_key
+  }
+
+  scheduling_policy { preemptible = var.vms_resources.preemptible }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat = var.vms_resources.nat
+  }
+}
